17b266638e69d7c88ff82a9539063da0b5aec21c
Sifat Ibna Amin <1605112@ugrad.cse.buet.ac.bd>
2021-02-19 00:57:04 +0600

    added provider functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:shop_app_flutter/screens/product_overview_screen.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app_flutter/providers/products.dart';
 
-void main() {
-  runApp(MyApp());
-}
+import './screens/product_detail_screen.dart';
+import './screens/product_overview_screen.dart';
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor: Colors.deepOrange,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: ProductOverviewScreen(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('My Shop'),
-      ),
-      body: Center(
-        child: Text('Home Page!'),
-      ),
+    return ChangeNotifierProvider(
+      create: (ctx) => Products(),
+      child: MaterialApp(
+          title: 'MyShop',
+          theme: ThemeData(
+            primarySwatch: Colors.purple,
+            accentColor: Colors.deepOrange,
+            fontFamily: 'Lato',
+          ),
+          home: ProductOverviewScreen(),
+          routes: {
+            ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+          }),
     );
   }
 }
