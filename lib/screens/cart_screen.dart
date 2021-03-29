de97ea3f45a74d58714fbedf3138e2b7ebca4495
Sifat Ibna Amin <1605112@ugrad.cse.buet.ac.bd>
2021-03-29 16:18:42 +0600

    applied order now functionality and saved it to order model

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_app_flutter/providers/cart.dart' show Cart;
+import 'package:shop_app_flutter/providers/orders.dart';
 import 'package:shop_app_flutter/widgets/cart_item.dart';
 
 class CartScreen extends StatelessWidget {
@@ -31,7 +32,7 @@ class CartScreen extends StatelessWidget {
                   Spacer(),
                   Chip(
                     label: Text(
-                      '${cart.totalAmount.toString()}',
+                      '${cart.totalAmount.toStringAsFixed(2)}',
                       style: TextStyle(
                         color:
                             Theme.of(context).primaryTextTheme.headline6.color,
@@ -39,10 +40,21 @@ class CartScreen extends StatelessWidget {
                     ),
                     backgroundColor: Theme.of(context).primaryColor,
                   ),
-                  FlatButton(
-                    onPressed: () {},
-                    child: Text('ORDER NOW'),
-                    textColor: Theme.of(context).primaryColor,
+                  TextButton(
+                    onPressed: () {
+                      Provider.of<Orders>(context, listen: false).addOrder(
+                        cart.items.values.toList(),
+                        cart.totalAmount,
+                      );
+                      cart.clear();
+                    },
+                    child: Text(
+                      'ORDER NOW',
+                      style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                      ),
+                    ),
+                    // textColor: Theme.of(context).primaryColor,
                   ),
                 ],
               ),
