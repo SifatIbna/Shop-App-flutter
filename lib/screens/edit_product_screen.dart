cf6acf14ceccf0c77002f11c00dddb2878371f76
Sifat Ibna Amin <1605112@ugrad.cse.buet.ac.bd>
2021-04-14 17:00:24 +0600

    replaced then method with try catch and finally method

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -81,7 +81,7 @@ class _EditProductScreenState extends State<EditProductScreen> {
     }
   }
 
-  void _saveForm() {
+  void _saveForm() async {
     final isValid = _form.currentState.validate();
     if (isValid) {
       _form.currentState.save();
@@ -96,10 +96,11 @@ class _EditProductScreenState extends State<EditProductScreen> {
         });
         Navigator.of(context).pop();
       } else {
-        Provider.of<Products>(context, listen: false)
-            .addProduct(_editedProduct)
-            .catchError((error) {
-          return showDialog<Null>(
+        try {
+          await Provider.of<Products>(context, listen: false)
+              .addProduct(_editedProduct);
+        } catch (error) {
+          await showDialog(
             context: context,
             builder: (ctx) => AlertDialog(
               title: Text('An error occured!'),
@@ -113,12 +114,12 @@ class _EditProductScreenState extends State<EditProductScreen> {
               ],
             ),
           );
-        }).then((_) {
+        } finally {
           setState(() {
             _isLoading = false;
           });
           Navigator.of(context).pop();
-        });
+        }
       }
     }
   }
