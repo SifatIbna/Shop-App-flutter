e765cb6df38be651a793c881147b846d01a380b5
Sifat Ibna Amin <1605112@ugrad.cse.buet.ac.bd>
2021-04-24 23:55:22 +0600

    added and fetched order firebase

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/foundation.dart';
+import 'package:shop_app_flutter/models/OrderJsonModel.dart';
+import 'package:shop_app_flutter/models/http_exceptions.dart';
+import '../providers/cart.dart' show CartItem;
 
 import 'cart.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class OrderItem {
   final String id;
@@ -24,15 +29,82 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
-  void addOrder(List<CartItem> cartProducts, double total) {
-    _orders.insert(
+  Future<void> fetchAndSetOrders() async {
+    const url =
+        "https://shop-app-project-cb096-default-rtdb.firebaseio.com/orders.json";
+
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode >= 400) {
+      throw HttpException(message: "Something Went Wrong!");
+    }
+
+    final List<OrderItem> loadedOrders = [];
+    final extractData = json.decode(response.body) as Map<String, dynamic>;
+    extractData.forEach(
+      (orderId, orderData) {
+        loadedOrders.add(
+          OrderItem(
+            id: orderId,
+            amount: orderData['amount'],
+            products: (orderData['products'] as List<dynamic>)
+                .map(
+                  (item) => CartItem(
+                    id: item['id'],
+                    price: item['price'],
+                    quantity: item['quantity'],
+                    title: item['title'],
+                  ),
+                )
+                .toList(),
+            dateTime: DateTime.parse(orderData['dateTime']),
+          ),
+        );
+      },
+    );
+    _orders = loadedOrders.reversed.toList();
+    notifyListeners();
+
+    // OrderModel orderModel = OrderModel.fromRawJson(response.body.toString());
+  }
+
+  Future<void> addOrder(List<CartItem> cartProducts, double total) async {
+    const url =
+        "https://shop-app-project-cb096-default-rtdb.firebaseio.com/orders.json";
+    final timeStamp = DateTime.now();
+    try {
+      final response = await http.post(
+        Uri.parse(url),
+        body: json.encode(
+          {
+            'amount': total,
+            'dateTime': timeStamp.toIso8601String(),
+            'products': cartProducts
+                .map(
+                  (e) => {
+                    'id': e.id,
+                    'title': e.title,
+                    'price': e.price,
+                    'quantity': e.quantity,
+                  },
+                )
+                .toList(),
+          },
+        ),
+      );
+      _orders.insert(
         0,
         OrderItem(
-          id: DateTime.now().toString(),
+          id: json.decode(response.body)['name'],
           amount: total,
-          dateTime: DateTime.now(),
+          dateTime: timeStamp,
           products: cartProducts,
-        ));
-    notifyListeners();
+        ),
+      );
+      notifyListeners();
+    } catch (error) {
+      print(error);
+      throw error;
+    }
   }
 }
